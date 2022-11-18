a8d2a10f378f4ca530901ad8afbd24b523fa3e04
Denis Sipakov <denis.sipakov@pandadoc.com>
2022-11-18 14:56:16 +0100

    Currency card UI

--- components/currencies/CurrencyCard.tsx
+++ components/currencies/CurrencyCard.tsx
@@ -6,14 +6,10 @@ import { styled, useTheme } from '@mui/material/styles';
 import { Avatar, Box, Card, Grid, Menu, MenuItem, Typography } from '@mui/material';
 
 // assets
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import GetAppTwoToneIcon from '@mui/icons-material/GetAppOutlined';
-import FileCopyTwoToneIcon from '@mui/icons-material/FileCopyOutlined';
-import PictureAsPdfTwoToneIcon from '@mui/icons-material/PictureAsPdfOutlined';
-import ArchiveTwoToneIcon from '@mui/icons-material/ArchiveOutlined';
+import NorthEastIcon from '@mui/icons-material/NorthEast';
+import SouthEastIcon from '@mui/icons-material/SouthEast';
 
-import { Rates } from './types';
+import { Rate } from './types';
 
 interface Types {
   currency: Currency,
@@ -42,7 +38,7 @@ const CardWrapper = styled(Card)(({ theme, selected }) => ({
     [theme.breakpoints.down('sm')]: {
       top: -105,
       right: -140
-    }
+    },
   },
   '&:before': {
     content: '""',
@@ -57,7 +53,7 @@ const CardWrapper = styled(Card)(({ theme, selected }) => ({
     [theme.breakpoints.down('sm')]: {
       top: -155,
       right: -70
-    }
+    },
   }
 }));
 
@@ -67,15 +63,37 @@ const CurrencyCard: FC<Types> = ({ currency, rates, selectCurrency, unselectCurr
 
   const [selected, setSelected] = useState<boolean>(false);
 
-  const getRate = (uuid: string): Rate => {
-    const latestRate: Rate = rates?.filter(
+  const getRate = (uuid: string, seqNumber: number = 0): Rate | undefined => {
+    if (!rates) {
+      return;
+    }
+
+    const currencyRates: Rate[] = rates.filter(
       (rate: Rate) => rate.currency === uuid
-    )[0];
-    return latestRate;
+    )
+    if (currencyRates.length < 2) {
+      return currencyRates[0];
+    }
+
+    return currencyRates[seqNumber];
+  }
+
+  const getPercentage = (uuid: string): number => {
+    if (!rates) {
+      return 0;
+    }
+
+    const _rates = rates.filter((rate: Rate) => rate.currency === uuid);
+    if (_rates.length < 2) {
+      return 0;
+    }
+
+    return (1 - (_rates[1].rate / _rates[0].rate)) * 100;
   }
 
   const handleClick = (): void => {
     setSelected(!selected);
+
     if (selected) {
       unselectCurrency(currency.code);
     } else {
@@ -102,26 +120,46 @@ const CurrencyCard: FC<Types> = ({ currency, rates, selectCurrency, unselectCurr
               </Grid>
             </Grid>
             <Grid item>
-              <Grid container alignItems="center">
+              <Grid container alignItems="baseline">
                 <Grid item>
                   <Typography sx={{ fontSize: '2.125rem', fontWeight: 500, mr: 1, mt: 1.75 }}>
-                    {getRate(currency.uuid)?.rate}
+                    {getRate(currency.uuid)?.rate.toFixed(4)}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography sx={{ fontSize: '1.125rem', fontWeight: 500, mr: 1 }}>
+                    {getRate(currency.uuid, 1)?.rate.toFixed(4)}
                   </Typography>
                 </Grid>
               </Grid>
-              <Typography>1.2%</Typography>
+              <Grid container alignItems="center" gap={1}>
+                <Grid item>
+                  <Typography>
+                    {getPercentage(currency.uuid)?.toFixed(2)}%
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography sx={{ fontSize: '.9rem' }}>
+                    ({(getRate(currency.uuid)?.rate - getRate(currency.uuid, 1)?.rate).toFixed(4)})
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  {getPercentage(currency.uuid) < 0 && <SouthEastIcon sx={{ color: 'red' }} />}
+                  {getPercentage(currency.uuid) > 0 && <NorthEastIcon sx={{ color: 'lightgreen' }} />}
+                </Grid>
+              </Grid>
             </Grid>
-            <Grid item>
-            </Grid>
-            <Grid item sx={{ mb: 1.25 }}>
+            <Grid item sx={{ mb: 1.25, selfAlign: "flex-end" }}>
               <Typography
                 sx={{
-                  fontSize: '1rem',
+                  fontSize: '.8rem',
                   fontWeight: 500,
-                  color: theme.palette.secondary[200]
+                  color: theme.palette.secondary[200],
+                  display: 'flex',
+                  justifyContent: 'flex-end'
                 }}
               >
-                {getRate(currency.uuid).rateDate}
+                {getRate(currency.uuid)?.rateDate}
               </Typography>
             </Grid>
           </Grid>
