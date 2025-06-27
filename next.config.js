ddaf9a01cf76d38c90fa44652806fa8255c4af9f
Denis Sipakov <mc.flyer@gmail.com>
2025-06-27 14:52:52 +0200

    Update dependencies and Next.js configuration
    - Add new package dependencies in package.json
    - Update yarn.lock with new dependency versions
    - Enhance next.config.js configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
@@ -10,7 +10,32 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return {
+      beforeFiles:
+        [
+          {
+            source: '/',
+            destination: '/', // Keep all other paths for app subdomain
+            has: [
+              {
+                type: 'host',
+                value: 'ispentadollar.com', // Only for subdomain
+              },
+            ],
+          },
+          {
+            source: '/',
+            destination: '/dashboard', // Keep all other paths for app subdomain
+            has: [
+              {
+                type: 'host',
+                value: 'app.ispentadollar.com', // Only for subdomain
+              },
+            ],
+          },
+        ]
+    }
   }
 }
-
-module.exports = nextConfig
