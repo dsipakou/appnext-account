d8c404d59d788e1e7e58a348abc202f49f452cae
Denis <mc.flyer@gmail.com>
2026-07-15 11:48:55 +0200

    Migrated to pnpm

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,18 @@
-# Install dependencies only when needed
 FROM node:22-alpine AS builder
-# Check https://github.com/nodejs/docker-node/tree/b4117f9333da4138b03a546ec926ef50a31506c3#nodealpine to understand why libc6-compat might be needed.
+
 RUN apk add --no-cache libc6-compat
+
 WORKDIR /app
+
+RUN corepack enable
+
+COPY package.json pnpm-lock.yaml ./
+RUN pnpm install --frozen-lockfile
+
 COPY . .
-RUN yarn install --frozen-lockfile
 
-# If using npm with a `package-lock.json` comment out above and use below instead
-# RUN npm ci
+ENV NEXT_TELEMETRY_DISABLED=1
 
-ENV NEXT_TELEMETRY_DISABLED 1
-
-# Add `ARG` instructions below if you need `NEXT_PUBLIC_` variables
-# then put the value on your fly.toml
 ARG NEXT_PUBLIC_ENV_TYPE
 ARG NEXT_PUBLIC_API_HOST
 ARG NEXT_PUBLIC_APP_HOST
@@ -20,17 +20,14 @@ ARG NEXT_PUBLIC_API_PORT
 ARG NEXT_PUBLIC_API_SCHEMA
 ARG NEXT_PUBLIC_NEXTAUTH_URL
 
-RUN yarn build
+RUN pnpm build
 
-# If using npm comment out above and use below instead
-# RUN npm run build
-
-# Production image, copy all the files and run next
 FROM node:22-alpine AS runner
+
 WORKDIR /app
 
-ENV NODE_ENV production
-ENV NEXT_TELEMETRY_DISABLED 1
+ENV NODE_ENV=production
+ENV NEXT_TELEMETRY_DISABLED=1
 
 RUN addgroup --system --gid 1001 nodejs
 RUN adduser --system --uid 1001 nextjs
@@ -39,7 +36,4 @@ COPY --from=builder /app ./
 
 USER nextjs
 
-CMD ["yarn", "start"]
-
-# If using npm comment out above and use below instead
-# CMD ["npm", "run", "start"]
+CMD ["pnpm", "start"]
--- components/transactions/forms/components/index.ts
+++ components/transactions/forms/components/index.ts
@@ -1,29 +1,21 @@
-import AccountComponent from './AccountComponent'
-import AccountReadComponent from './AccountReadComponent'
-import AccountReadComponentV2 from './AccountReadComponent'
-import ActionsReadComponent from './ActionsReadComponent'
-import AmountComponent from './AmountComponent'
-import AmountReadComponent from './AmountReadComponent'
-import AmountReadComponentV2 from './AmountReadComponentV2'
-import BaseAmountReadComponent from './BaseAmountReadComponent'
-import BudgetComponent from './BudgetComponent'
-import BudgetReadComponent from './BudgetReadComponent'
-import BudgetReadComponentV2 from './BudgetReadComponentV2'
-import CategoryComponent from './CategoryComponent'
-import CategoryComponentV2 from './CategoryComponentV2'
-import CategoryReadComponent from './CategoryReadComponent'
-import CategoryReadComponentV2 from './CategoryReadComponentV2'
-import CurrencyComponent from './CurrencyComponent'
-import DateComponent from './DateComponent'
-import DateComponentV2 from './DateComponentV2'
-import DateReadComponent from './DateReadComponent'
-import DateReadComponentV2 from './DateReadComponentV2'
-import DescriptionComponent from './DescriptionComponent'
-import MainCurrencyAmountReadComponent from './MainCurrencyAmountReadComponents'
+import AccountReadComponentV2 from './AccountReadComponentV2';
+import ActionsReadComponent from './ActionsReadComponent';
+import AmountComponent from './AmountComponent';
+import AmountReadComponent from './AmountReadComponent';
+import AmountReadComponentV2 from './AmountReadComponentV2';
+import BaseAmountReadComponent from './BaseAmountReadComponent';
+import BudgetComponent from './BudgetComponent';
+import BudgetReadComponentV2 from './BudgetReadComponentV2';
+import CategoryComponentV2 from './CategoryComponentV2';
+import CategoryReadComponent from './CategoryReadComponent';
+import CategoryReadComponentV2 from './CategoryReadComponentV2';
+import CurrencyComponent from './CurrencyComponent';
+import DateComponent from './DateComponent';
+import DateComponentV2 from './DateComponentV2';
+import DateReadComponent from './DateReadComponent';
+import DateReadComponentV2 from './DateReadComponentV2';
 
 export {
-  AccountComponent,
-  AccountReadComponent,
   AccountReadComponentV2,
   ActionsReadComponent,
   AmountComponent,
@@ -31,9 +23,7 @@ export {
   AmountReadComponentV2,
   BaseAmountReadComponent,
   BudgetComponent,
-  BudgetReadComponent,
   BudgetReadComponentV2,
-  CategoryComponent,
   CategoryComponentV2,
   CategoryReadComponent,
   CategoryReadComponentV2,
@@ -42,6 +32,4 @@ export {
   DateComponentV2,
   DateReadComponent,
   DateReadComponentV2,
-  DescriptionComponent,
-  MainCurrencyAmountReadComponent,
-}
+};
