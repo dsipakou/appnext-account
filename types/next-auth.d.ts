075d15228dc024cb6f14a3e482dee215b3190e8b
Denis <mc.flyer@gmail.com>
2026-08-07 12:36:46 +0200

    Fix format

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,12 +1,11 @@
-import NextAuth, { DefaultSession } from "next-auth"
+import { DefaultSession } from 'next-auth';
 
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session {
     user: {
-      currency: string
-      username: string
-      token: string
-    }
-  } & DefaultSession["user"]
+      currency: string;
+      username: string;
+      token: string;
+    } & DefaultSession['user'];
+  }
 }
-
