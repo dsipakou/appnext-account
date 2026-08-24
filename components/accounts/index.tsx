2286e4c19d21711ee89d2339e52424b2349c6ef4
Denis <mc.flyer@gmail.com>
2026-08-24 13:32:19 +0200

    Loading screen for account

--- components/accounts/index.tsx
+++ components/accounts/index.tsx
@@ -2,6 +2,7 @@ import { useSession } from "next-auth/react";
 import * as React from "react";
 
 import AccountCard from "@/components/accounts/components/AccountCard";
+import { Spinner } from "@/components/ui/spinner";
 import { User } from "@/components/users/types";
 import { useAccounts } from "@/hooks/accounts";
 import { useUsers } from "@/hooks/users";
@@ -10,7 +11,7 @@ import { AddForm as AddAccount } from "./forms";
 import { AccountResponse } from "./types";
 
 const Index: React.FC = () => {
-  const { data: accounts = [] } = useAccounts();
+  const { data: accounts = [], isLoading: isAccountsLoading } = useAccounts();
   const { data: session } = useSession();
   const { data: users = [] } = useUsers();
 
@@ -25,13 +26,6 @@ const Index: React.FC = () => {
   const otherAccounts = authUser
     ? accounts.filter((item: AccountResponse) => item.user !== authUser.uuid)
     : accounts;
-  const sortedAccounts = [...yourAccounts, ...otherAccounts];
-
-  const noAccounts = (
-    <div className="flex flex-1 items-center justify-center">
-      <span className="text-2xl">No accounts added</span>
-    </div>
-  );
 
   return (
     <div className="flex flex-1 flex-col gap-2 overflow-y-auto">
@@ -39,27 +33,41 @@ const Index: React.FC = () => {
         <span className="text-xl font-semibold">Accounts</span>
         <AddAccount />
       </div>
-      {sortedYourAccounts.length === 0 && noAccounts}
-      <div className="rounded-xl bg-white pt-3">
-        <span className="p-3">Your accounts</span>
-        <div className="flex flex-col rounded-xl bg-white py-3">
-          {sortedYourAccounts.map((item: AccountResponse) => (
-            <div key={item.uuid}>
-              <AccountCard account={item} />
-            </div>
-          ))}
+      {isAccountsLoading && (
+        <div className="flex h-full items-center justify-center">
+          <div className="text-center">
+            <Spinner className="size-8" />
+          </div>
         </div>
-      </div>
-      <div className="rounded-xl bg-white pt-3">
-        <span className="p-3">Other accounts</span>
-        <div className="flex flex-col rounded-xl bg-white py-3">
-          {otherAccounts.map((item: AccountResponse) => (
-            <div key={item.uuid}>
-              <AccountCard account={item} />
-            </div>
-          ))}
+      )}
+      {!isAccountsLoading && sortedYourAccounts.length === 0 ? (
+        <div className="flex flex-1 items-center justify-center">
+          <span className="text-2xl">No accounts added</span>
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="rounded-xl bg-white pt-3">
+            <span className="p-3">Your accounts</span>
+            <div className="flex flex-col rounded-xl bg-white py-3">
+              {sortedYourAccounts.map((item: AccountResponse) => (
+                <div key={item.uuid}>
+                  <AccountCard account={item} />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="rounded-xl bg-white pt-3">
+            <span className="p-3">Other accounts</span>
+            <div className="flex flex-col rounded-xl bg-white py-3">
+              {otherAccounts.map((item: AccountResponse) => (
+                <div key={item.uuid}>
+                  <AccountCard account={item} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
